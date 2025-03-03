8df8a921d228d65f19ed04a047bdaafa2664f89e
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-03-04 00:15:13 +0100

    refactor:  update prisma client methods

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,14 +45,11 @@ exports.signup = catchAsync(async (req, res, next) => {
     speciality,
   } = req.body;
 
-  // Validate role value
   if (role && !["student", "teacher", "entreprise"].includes(role)) {
     return next(
       new AppError("Role must be one of: student, teacher, or entreprise", 400)
     );
   }
-
-  // Create the user and, if role is student, also create a Student record.
 
   const newUser = await prisma.user.signup({
     data: {
@@ -76,78 +73,3 @@ exports.signup = catchAsync(async (req, res, next) => {
 
   createSendToken(newUser, 201, res);
 });
-
-exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body;
-
-  // 1) Check if email and password exist
-  if (!email || !password) {
-    return next(new AppError("Please provide email and password", 400));
-  }
-  // 2) Check if the user exists and the password is correct
-  const user = await User.findOne({ email }).select("+password");
-
-  if (!user || !(await user?.correctPassword(password, user.password))) {
-    return next(new AppError("Incorrect email or password", 401)); //unauthorized
-  }
-  // 3) If everything ok, send token to client
-  createSendToken(user, 200, res);
-});
-
-exports.logout = catchAsync(async (req, res, next) => {
-  res.clearCookie("jwt", {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // Adjust for cross-site cookies
-    path: "/",
-  });
-  res.status(200).json({ status: "success" });
-});
-
-exports.protect = catchAsync(async (req, res, next) => {
-  // 1) Getting token and check if it is exists
-
-  const token = req.cookies.jwt;
-  if (!token) {
-    return next(
-      new AppError("You are not logged in! Please log in to get access.", 401)
-    );
-  }
-
-  // 2) Verification token
-
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-
-  // 3) Check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser)
-    next(
-      new AppError(
-        "The user belonging to this token does no longer exist.",
-        401
-      )
-    );
-
-  // 4) Check if user changed password after the JWT token was issued
-
-  if (freshUser?.changedPasswordAfter(decoded.iat)) {
-    return next(
-      new AppError("User recently changed password! Please log in again", 401)
-    );
-  } // iat is the time the token was issued
-
-  // Grant access to protected route
-  req.user = freshUser;
-  next();
-});
-
-exports.restrictTo = (...roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError("You do not have permission to perform this action", 403)
-      );
-    }
-    next();
-  };
-};
--- prisma/prismaClient.js
+++ prisma/prismaClient.js
@@ -9,15 +9,15 @@ const prisma = new PrismaClient().$extends({
           console.log("password", data.password);
           data.password = await bcrypt.hash(data.password, 12);
         }
-        return await prisma.user.create({ data }); // Use the original Prisma create method
+        return await prisma.user.create({ data });
       },
 
-      async update(args) {
+      async updateUser(args) {
         if (args.data.password) {
           args.data.password = await bcrypt.hash(args.data.password, 12);
           args.data.passwordChangedAt = new Date(Date.now() - 1000);
         }
-        return this.$apply.client.user.update(args); // Use the original Prisma update method
+        return await prisma.user.update(args);
       },
     },
   },
