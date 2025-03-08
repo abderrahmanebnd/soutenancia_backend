328b2d438ba65f2923cfacd080275e79c5aad5fd
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-03-08 18:09:22 +0100

    trying to fix the cookies persitance issue

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,6 +22,7 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     path: "/",
+    domain: "localhost",
   };
 
   res.cookie("jwt", token, cookieOptions);
